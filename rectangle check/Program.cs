52a74014e4d16a52800aab77daa8afbee23fb0cf
Safty952003 <safty952003@gmail.com>
2024-07-28 01:49:33 +0300

    Add project files.

---
+++ rectangle check/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace rectangle_check
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the length of the rectangle: ");
+            double length = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the width of the rectangle: ");
+            double width = double.Parse(Console.ReadLine());
+
+            if (length == width)
+            {
+                Console.WriteLine("The rectangle is a square.");
+            }
+            else
+            {
+                Console.WriteLine("The rectangle is not a square.");
+            }
+        }
+    }
+}
