3d3d6eaf37f722aef0eb640532d3a2754fcbcb1a
Safty952003 <safty952003@gmail.com>
2024-07-28 01:42:09 +0300

    Add project files.

---
+++ even odd check/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace even_odd_check
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("The number is even.");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd.");
+            }
+        }
+    }
+}
