eb18ac8da13491a90c21a5a5eed073a813656a8a
Safty952003 <safty952003@gmail.com>
2024-07-28 00:47:23 +0300

    Add project files.

---
+++ student/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace student
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your Name:");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your ID:");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter tour age:");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter your depatment:");
+            string dept = Console.ReadLine();
+
+            Console.WriteLine("**************");
+
+            Console.WriteLine($"your Name is:{name}");
+            Console.WriteLine($"your age is:{age}");
+            Console.WriteLine($"your ID is:{id}");
+            Console.WriteLine($"your depatment is:{dept}");
+
+        }
+    }
+}
